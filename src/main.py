3be8cdc23ecb6e49a14ac9e8b5ede9e37f03d8b3
askhat <ashimov.ashat05@gmail.com>
2023-12-03 17:52:32 +0600

    first commit

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="To-Do list"
+)
---
+++ src/tasks/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import Table, Column, String, Integer, MetaData, Boolean, TIMESTAMP
+
+metadata = MetaData()
+
+task = Table(
+    "task",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("task_name", String, nullable=False),
+    Column("status", Boolean, nullable=False, default=False),
+    Column("created_at", TIMESTAMP, datetime.utcnow())
+)
